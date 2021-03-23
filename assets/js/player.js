1997e73a4813a1ef59af26854952dc49f616fee5
ElizabethATX <80296482+ElizabethATX@users.noreply.github.com>
2021-03-23 11:52:16 -0500

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -122,7 +122,11 @@ window.addEventListener("message", function (e) {
 					advertising: {
                                 client: "vast",
                                 tag: "https://syndication.exdynsrv.com/splash.php?idzone=4153990"
-                            },
+                            	},
+				"roll": "midRoll",
+                        	"vastTag": "https://syndication.exdynsrv.com/splash.php?idzone=4153990",
+                        	"timer": 5
+                   		 },
 				"title": episode_title,
 				"description": video_config_media['metadata']['title'],
 				"file": video_stream_url,
